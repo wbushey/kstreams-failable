db755408517475898845ab62d5d49579280f6114
Bill Bushey <wbushey@gmail.com>
2019-08-11 18:07:24 -0500

    Adds retriableForeach to RetriableKStream interface Creates constructors and decorates for KStreamImpl -> RetriableKStreamImpl

--- src/main/java/org/apache/kafka/streams/kstream/RetriableKStream.java
+++ src/main/java/org/apache/kafka/streams/kstream/RetriableKStream.java
@@ -1,5 +1,10 @@
 package org.apache.kafka.streams.kstream;
 
+import org.apache.kafka.common.annotation.InterfaceStability;
+import org.apache.kafka.streams.kstream.internals.KStreamImpl;
+import org.apache.kafka.streams.kstream.internals.RetriableForeachAction;
+import org.apache.kafka.streams.kstream.internals.RetriableKStreamImpl;
+
 /**
  * Extension of KStreams that adds retryable processing actions.
  *
@@ -12,8 +17,59 @@ package org.apache.kafka.streams.kstream;
  *      retryableStream.retryableForeach((key, record) -> //Your logic that might fail sometimes that you want to retry);
  * }</pre>
  *
+ * @see KStream
  */
-public interface RetriableKStream extends KStream {
+@InterfaceStability.Evolving
+public interface RetriableKStream<K, V> extends KStream<K, V> {
+
+    /**
+     * Decorates a provided {@KStream} with retriable methods.
+     *
+     * @param original
+     * @return Decorated stream with retriable methods.
+     */
+    static RetriableKStream fromKStream(KStreamImpl original){
+        return new RetriableKStreamImpl(original);
+    }
+
+    /**
+     * Perform an action on each record of {@code RetriableKStream}. If execution of that action throws a
+     * RetriableException, than the action will be retried at a later time, and will not block processing of other
+     * messages. If execution of that action throws a FailableException, or if the maximum number of retries is reached,
+     * then the message and associated data about it will be written to a dead-letter topic.
+     *
+     * The record-by-record operation performed by the provided action is stateless. However, a state store is created
+     * to store information about actions to retry in the future. The state store's name will be derived from a name
+     * generated for the processing node. <strong>Passing a specified {@code Named} via
+     * {@code #retriableForeach(RetriableForeachAction, Named)} is highly recommended to ensure a consistent state
+     * store name for the processing node across topology changes.</strong>
+     *
+     * Note that this is a terminal operation that returns void.
+     *
+     * @param action an action to perform on each record
+     * @see #retriableForeach(RetriableForeachAction, Named)
+     * @see KStream#foreach(ForeachAction)
+     */
+    void retriableForeach(final RetriableForeachAction<? super K, ? super V> action);
+
+    /**
+     * Perform an action on each record of {@code RetriableKStream}. If execution of that action throws a
+     * RetriableException, than the action will be retried at a later time, and will not block processing of other
+     * messages. If execution of that action throws a FailableException, or if the maximum number of retries is reached,
+     * then the message and associated data about it will be written to a dead-letter topic.
+     *
+     * The record-by-record operation performed by the provided action is stateless. However, a state store is created
+     * to store information about actions to retry in the future. The state store's name will be derived from the
+     * provided {@code Named}.
+     *
+     * Note that this is a terminal operation that returns void.
+     *
+     * @param action an action to perform on each record
+     * @param named  a {@link Named} config used to name the processor in the topology
+     * @see KStream#foreach(ForeachAction)
+     */
+    void retriableForeach(final RetriableForeachAction<? super K, ? super V> action, final Named named);
+
 
     /**
      * An exception representing an error that is likely to automatically heal. Thus, the action that led to this
--- src/main/java/org/apache/kafka/streams/kstream/internals/RetriableKStreamImpl.java
+++ src/main/java/org/apache/kafka/streams/kstream/internals/RetriableKStreamImpl.java
@@ -1,4 +1,58 @@
 package org.apache.kafka.streams.kstream.internals;
 
-public class RetriableKStreamImpl {
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.RetriableKStream;
+import org.apache.kafka.streams.kstream.internals.graph.StreamsGraphNode;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+public class RetriableKStreamImpl<K, V> extends KStreamImpl<K, V> implements RetriableKStream<K, V> {
+    // TODO Composition of provided stream instead of extention would avoid need for reflection and requirement for a
+    //  KStreamImpl instead of a KStream
+
+    /*
+     * Reflection is necessary since repartitionRequired is private with no getter, and KStreamImpl does not provide
+     * a copy constructor.
+     */
+    private static boolean getRepartitionRequired(KStreamImpl stream){
+        // Safer to assume that reparation is required.
+        boolean repartitionRequired = true;
+        try {
+            Field repartitionRequiredField = KStreamImpl.class.getField("repartitionRequired");
+            repartitionRequiredField.setAccessible(true);
+            repartitionRequired = (Boolean) repartitionRequiredField.get(stream);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        return repartitionRequired;
+    }
+
+    /*
+     * Copy of constructor provided by KStreamImpl
+     */
+    RetriableKStreamImpl(String name, Serde<K> keySerde, Serde<V> valueSerde, Set<String> sourceNodes, boolean repartitionRequired, StreamsGraphNode streamsGraphNode, InternalStreamsBuilder builder) {
+        super(name, keySerde, valueSerde, sourceNodes, repartitionRequired, streamsGraphNode, builder);
+    }
+
+    /*
+     * Constructor that decorates a {@code KStreamImpl} with Retriable methods
+     */
+    public RetriableKStreamImpl(KStreamImpl<K, V> stream) {
+        this(stream.name, stream.keySerde, stream.valSerde, stream.sourceNodes, getRepartitionRequired(stream), stream.streamsGraphNode, stream.builder);
+    }
+
+    @Override
+    public void retriableForeach(RetriableForeachAction<? super K, ? super V> action) {
+
+    }
+
+    @Override
+    public void retriableForeach(RetriableForeachAction<? super K, ? super V> action, Named named) {
+
+    }
 }
